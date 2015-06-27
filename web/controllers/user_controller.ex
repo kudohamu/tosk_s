2c96db610f6d900c5dd948d6c6194b32eb85f31e
kudohamu <kudohamu91086@yahoo.co.jp>
2015-06-27 14:03:31 +0900

    Userモデルにupload_iconメソッドを追加

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -13,48 +13,35 @@ defmodule Tosk.UserController do
 
   def create(conn, %{"user" => user_params}) do
     if (user_params["provider"] == "own") do
-      ext = Enum.at(String.split(Enum.at(String.split(user_params["icon_data"], "data:image/"), 1), ";"), 0)
-      if (User.valid_image_ext?(ext)) do
-        image_b64 = Enum.at(String.split(user_params["icon_data"], "base64,"), 1)
-        case {:ok, image_bin} = Base.decode64(image_b64) do
-          {:ok, image_bin} ->
-            icon_name = "#{Ecto.UUID.generate()}.#{ext}"
-            case File.write(Path.join(User.get_user_icon_path, icon_name), image_bin, [:binary]) do
-              :ok ->
-                user_params = Map.put(user_params, "icon", Path.join(User.get_user_icon_path, icon_name))
+      case User.upload_icon(user_params["icon_data"]) do
+        {:ok, icon_name} ->
+          user_params = Map.put(user_params, "icon", Path.join(User.get_user_icon_path, icon_name))
 
-                hashed_password = Comeonin.Bcrypt.hashpwsalt(user_params["password"])
-                user_params = Map.put(user_params ,"hashed_password", hashed_password)
+          hashed_password = Comeonin.Bcrypt.hashpwsalt(user_params["password"])
+          user_params = Map.put(user_params ,"hashed_password", hashed_password)
 
-                user_params = Map.put(user_params, "uid", Ecto.UUID.generate())
+          user_params = Map.put(user_params, "uid", Ecto.UUID.generate())
 
-                changeset = User.changeset(%User{}, user_params)
-                if changeset.valid? && 7 < String.length(user_params["password"]) && user_params["password"] == user_params["password_confirmation"] do
-                  user = Repo.insert(changeset)
-                  token = Ecto.UUID.generate()
-                  conn = conn |> fetch_session |> put_session(:token, Comeonin.Bcrypt.hashpwsalt(token))
-                  render(conn, "create.json", %{id: user.id, token: token})
-                else
-                  # conn
-                  # |> put_status(:unprocessable_entity)
-                  # |> render(Tracoo.ChangesetView, "error.json", changeset: changeset)
-                  if (changeset.errors[:mail] != nil) do
-                    render(conn, "error.json", %{msg: "既に登録されているメールアドレスです"})
-                  else
-                    render(conn, "error.json", %{msg: "不正なデータが含まれています"})
-                  end
-                end
-              {:error, _} ->
-                # 画像が保存できなかった
-                render(conn, "error.json", %{msg: "画像を保存できませんでした"})
+          changeset = User.changeset(%User{}, user_params)
+          if changeset.valid? && 7 < String.length(user_params["password"]) && user_params["password"] == user_params["password_confirmation"] do
+            user = Repo.insert(changeset)
+            token = Ecto.UUID.generate()
+            conn = conn |> fetch_session |> put_session(:token, Comeonin.Bcrypt.hashpwsalt(token))
+            render(conn, "create.json", %{id: user.id, token: token})
+          else
+            # conn
+            # |> put_status(:unprocessable_entity)
+            # |> render(Tracoo.ChangesetView, "error.json", changeset: changeset)
+            if (changeset.errors[:mail] != nil) do
+              render(conn, "error.json", %{msg: "既に登録されているメールアドレスです"})
+            else
+              render(conn, "error.json", %{msg: "不正なデータが含まれています"})
             end
-          {:error, _} ->
-            # Base64からbinaryへの変換に失敗
-            render(conn, "error.json", %{msg: "画像データが正しくない可能性があります"})
-        end
-      else
-        # 画像の拡張子が不正
-        render(conn, "error.json", %{msg: "不正な画像データです"})
+          end
+        {:err, :invalid_data} ->
+          render(conn, "error.json", %{msg: "画像データが正しくない可能性があります"})
+        {:err, :cannot_save_icon} ->
+          render(conn, "error.json", %{msg: "画像を保存できませんでした"})
       end
     else
       # providerの値が不正
--- web/models/user.ex
+++ web/models/user.ex
@@ -12,8 +12,8 @@ defmodule Tosk.User do
     timestamps
   end
 
-  @required_fields ~w(icon name mail provider uid)
-  @optional_fields ~w(hashed_password)
+  @required_fields ~w(icon name mail provider)
+  @optional_fields ~w(hashed_password uid)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -25,8 +25,10 @@ defmodule Tosk.User do
     model
     |> cast(params, @required_fields, @optional_fields)
     |> validate_format(:name, ~r/^[0-9a-zA-Z_@\-]{4,30}$/)
+    |> validate_format(:icon, ~r/^.+\.(png|jpg|jpeg)$/)
     |> validate_format(:mail, ~r/^([\w\-\+_]+\.?[\w\-\+_]+)+@([a-z0-9\-]+\.[a-z]+)+$/)
     |> validate_unique(:mail, on: Tosk.Repo)
+    |> validate_format(:provider, ~r/^(own|twitter|facebook)$/)
   end
 
   def get_user_icon_path do
@@ -35,5 +37,32 @@ defmodule Tosk.User do
 
   def valid_image_ext?(ext) do
     Enum.member?(["jpg", "jpeg", "png"], ext)
+  end
+
+  def upload_icon(icon_data) do
+    ext = Enum.at(String.split(Enum.at(String.split(icon_data, "data:image/"), 1), ";"), 0)
+    if (valid_image_ext?(ext)) do
+      image_b64 = Enum.at(String.split(icon_data, "base64,"), 1)
+      case {:ok, image_bin} = Base.decode64(image_b64) do
+        {:ok, image_bin} ->
+          icon_name = "#{Ecto.UUID.generate()}.#{ext}"
+          unless File.exists?(get_user_icon_path) do
+            File.mkdir_p(get_user_icon_path)
+          end
+          case File.write(Path.join([get_user_icon_path, icon_name]), image_bin, [:binary]) do
+            :ok ->
+              {:ok, icon_name}
+            {:error, _} ->
+              # 画像が保存できなかった
+              {:err, :cannot_save_icon}
+          end
+        {:error, _} ->
+          # Base64からbinaryへの変換に失敗
+          {:err, :invalid_data}
+      end
+    else
+      # 画像の拡張子が不正
+      {:err, :invalid_data}
+    end
   end
 end
