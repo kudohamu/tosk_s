cb4059cad0aca4e05a2f25270880a2a4eb9729bc
kudohamu <kudohamu91086@yahoo.co.jp>
2015-07-11 16:46:32 +0900

    TODOモデルにバリデーションを追加

--- web/models/todo.ex
+++ web/models/todo.ex
@@ -1,6 +1,8 @@
 defmodule Tosk.TODO do
   use Tosk.Web, :model
 
+  alias Tosk.Board
+
   schema "todos" do
     field :uid, :string
     field :public, :boolean, default: false
@@ -10,8 +12,8 @@ defmodule Tosk.TODO do
     timestamps
   end
 
-  @required_fields ~w(uid public content)
-  @optional_fields ~w()
+  @required_fields ~w(uid content board_id)
+  @optional_fields ~w(public)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -23,5 +25,15 @@ defmodule Tosk.TODO do
     model
     |> cast(params, @required_fields, @optional_fields)
     |> validate_unique(:uid, on: Tosk.Repo)
+    |> validate_public_only_template()
+  end
+
+  def validate_public_only_template(changeset) do
+    board = Tosk.Repo.get_by(Board, id: get_field(changeset, :board_id))
+    if (board.category == 1 && get_field(changeset, :public)) do
+      add_error(changeset, :"active_board_todo", "cannot be public")
+    else 
+      changeset
+    end
   end
 end
