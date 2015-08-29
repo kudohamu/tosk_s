68d99a4b1226fafcdadb7b8803d41dfcd8491cd9
kudohamu <kudohamu91086@yahoo.co.jp>
2015-08-29 11:02:21 +0900

    changed: move todo channel action to board channel

--- web/channels/board_channel.ex
+++ web/channels/board_channel.ex
@@ -2,9 +2,12 @@ defmodule Tosk.BoardChannel do
   use Tosk.Web, :channel
 
   alias Tosk.Board
+  alias Tosk.TODO
   alias Tosk.UsersBoards
 
-  def join("boards:" <> _user_id, payload, socket) do
+  def join("boards:" <> _board_id, payload, socket) do
+    socket = assign(socket, :id, payload["id"])
+    socket = assign(socket, :token, payload["token"])
     if authorized?(payload) do
       {:ok, socket}
     else
@@ -19,7 +22,7 @@ defmodule Tosk.BoardChannel do
   end
 
   def handle_in("index", payload, socket) do
-    "boards:" <> _user_id = socket.topic
+    _user_id = socket.assigns.id
 
     ids_query = Ecto.Query.from ub in UsersBoards, 
     where: ub.user_id == ^_user_id,
@@ -41,7 +44,8 @@ defmodule Tosk.BoardChannel do
   end
 
   def handle_in("create", payload, socket) do
-    "boards:" <> _user_id = socket.topic
+    _user_id = socket.assigns.id
+
     changeset = Board.changeset(%Board{}, %{ name: payload["name"] })
     case Repo.transaction(fn ->
       if changeset.valid? do
@@ -66,11 +70,27 @@ defmodule Tosk.BoardChannel do
     {:noreply, socket}
   end
 
-  def handle_in("delete", payload, socket) do
-    "boards:" <> _user_id = socket.topic
+  def handle_in("update", payload, socket) do
+    "boards:" <> _board_id = socket.topic
 
-    board = Repo.get(Board, payload["id"])
-    usersboards = Repo.get_by(UsersBoards, user_id: _user_id, board_id: payload["id"])
+    board = Repo.get(Board, _board_id)
+    changeset = Board.changeset(board, %{ name: payload["name"] })
+
+    if changeset.valid? do
+      board = Repo.update(changeset)
+      broadcast socket, "updated", %{:board => %{ id: board.id, name: board.name }}
+    else
+      {:noreply, socket}
+    end
+    {:noreply, socket}
+  end
+
+  def handle_in("delete", payload, socket) do
+    "boards:" <> _board_id = socket.topic
+    _user_id = socket.assigns.id
+
+    board = Repo.get(Board, _board_id)
+    usersboards = Repo.get_by(UsersBoards, user_id: _user_id, board_id: _board_id)
 
     if board && usersboards do
       case Repo.transaction(fn -> 
@@ -111,6 +131,73 @@ defmodule Tosk.BoardChannel do
     push socket, event, payload
     {:noreply, socket}
   end
+
+
+  #############################
+  ###  TODOに関するhandler  ###
+  #############################
+
+  def handle_in("todo:index", payload, socket) do
+    "boards:" <> _board_id = socket.topic
+
+    todos_query = Ecto.Query.from td in TODO,
+      where: td.board_id == ^_board_id
+    todos = Repo.all(todos_query)
+    jtodo = Enum.map(todos,
+      fn todo ->
+        case TODO.encodeTODOtoJSON(todo.id) do
+          {:ok, jtodo} ->
+            jtodo
+          {:error} ->
+            %{}
+        end
+      end
+    )
+    broadcast socket, "todo:index", %{:todos => jtodo}
+    {:noreply, socket}
+  end
+
+  def handle_in("todo:change", payload, socket) do
+    case TODO.decodeJSONtoTODO(payload["todo"]) do
+      {:ok, todo} ->
+        {:ok, jtodo} = TODO.encodeTODOtoJSON(todo.id)
+        broadcast socket, "todo:changed", %{:todo => jtodo}
+      {:error} ->
+        {:noreply, socket}
+    end
+    {:noreply, socket}
+  end
+
+  def handle_in("todo:create", payload, socket) do
+    "boards:" <> _board_id = socket.topic
+
+    board = Repo.get(Board, _board_id)
+    if board do
+      changeset = TODO.changeset(%TODO{}, %{ uid: Ecto.UUID.generate(), title: payload["title"], checked: false, content: "[]", board_id: board.id })
+      if changeset.valid? do
+        todo = Repo.insert(changeset)
+        {:ok, jtodo} = TODO.encodeTODOtoJSON(todo.id)
+        broadcast socket, "todo:created", %{:todo => jtodo}
+      end
+    end
+    {:noreply, socket}
+  end
+
+  def handle_in("todo:delete", payload, socket) do
+    "boards:" <> _board_id = socket.topic
+
+    todo = Repo.get_by(TODO, uid:  payload["id"])
+    if todo && todo.board_id == String.to_integer(_board_id) do
+      case Repo.delete todo do
+        {:error, _} ->
+          {:noreply, socket}
+        _ ->
+          broadcast socket, "todo:deleted", %{:id => todo.uid}
+      end
+    end
+    {:noreply, socket}
+  end
+
 
   # Add authorization logic here as required.
   defp authorized?(_payload) do
--- web/channels/todo_channel.ex
+++ web/channels/todo_channel.ex
@@ -21,64 +21,6 @@ defmodule Tosk.TODOChannel do
 
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (todos:*).
-  def handle_in("index", payload, socket) do
-    "todos:" <> board_id = socket.topic
-    todos_query = Ecto.Query.from td in TODO, 
-      where: td.board_id == ^board_id
-    todos = Repo.all(todos_query)
-    jtodo = Enum.map(todos, 
-      fn todo -> 
-        case TODO.encodeTODOtoJSON(todo.id) do
-          {:ok, jtodo} ->
-            jtodo
-          {:error} ->
-            %{}
-        end
-      end)
-    broadcast socket, "index", %{:data => jtodo}
-    {:noreply, socket}
-  end
-
-  def handle_in("change", payload, socket) do
-    case TODO.decodeJSONtoTODO(payload["todo"]) do
-      {:ok, todo} ->
-        {:ok, jtodo} = TODO.encodeTODOtoJSON(todo.id)
-        broadcast socket, "changed", %{:todo => jtodo}
-      {:error} ->
-        {:noreply, socket}
-    end
-    {:noreply, socket}
-  end
-
-  def handle_in("create", payload, socket) do
-
-    "todos:" <> board_id = socket.topic
-    board = Repo.get(Board, board_id)
-    if board do
-      changeset = TODO.changeset(%TODO{}, %{ uid: Ecto.UUID.generate(), title: payload["title"], checked: false, content: "[]", board_id: board.id })
-      if changeset.valid? do
-        todo = Repo.insert(changeset)
-        {:ok, jtodo} = TODO.encodeTODOtoJSON(todo.id)
-        broadcast socket, "created", %{:todo => jtodo}
-      end
-    end
-    {:noreply, socket}
-  end
-
-  def handle_in("delete", payload, socket) do
-    "todos:" <> board_id = socket.topic
-    todo = Repo.get_by(TODO, uid:  payload["id"])
-    if todo && board_id == todo.board_id do
-      case Repo.delete todo do
-        {:error, _} ->
-          {:noreply, socket}
-        _ ->
-          broadcast socket, "deleted", %{:id => todo.uid}
-      end
-    end
-    {:noreply, socket}
-  end
-
   # This is invoked every time a notification is being broadcast
   # to the client. The default implementation is just to push it
   # downstream but one could filter or change the event.
